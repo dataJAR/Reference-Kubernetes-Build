5ce2d36ca9cd040cb080029eceaafbf9644b1931
macmule <ben@macmule.com>
2020-04-10 16:01:08 +0100

    Update setenv.sh
    Removed TLS as not needed with jamf pro 10.20+

--- build/jss/config/setenv.sh
+++ build/jss/config/setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CATALINA_HOME=/usr/local/tomcat
 # TLS 1.3 disabled as per: https://macmule.com/2019/10/01/more-dep-sync-errors/
-export CATALINA_OPTS="-Xms1024M -Xmx1024M -Djava.awt.headless=true -server -Djava.security.egd=file:/dev/./urandom -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true -Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2"
+export CATALINA_OPTS="-Xms1024M -Xmx1024M -Djava.awt.headless=true -server -Djava.security.egd=file:/dev/./urandom -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true"
 export JAVA_HOME="/usr/lib/jvm/java-11-amazon-corretto/"
 # https://stackoverflow.com/a/39625055
 export UMASK='0022'
